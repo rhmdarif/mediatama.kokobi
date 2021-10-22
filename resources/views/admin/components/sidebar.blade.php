aa8510eec82ad1948b56f0b4674042238545b37f
Rahmad Arif <rahmadarif28080@gmail.com>
2021-10-22 10:03:23 +0700

    add register user

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -14,19 +14,7 @@
           <img src="{{ asset('Admin/assets') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
+          <a href="#" class="d-block">{{ session('admin.username', '') }}</a>
         </div>
       </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,47 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+@extends('layouts.auth')
+@section('contents')
+<div class="card o-hidden border-0 shadow-lg my-5">
+    <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+            <div class="col-lg-6 d-none d-lg-block" style="background: url({{ asset('assets/images/login.png') }});background-position: center;background-size: cover;"></div>
+            <div class="col-lg-6">
+                <div class="p-5">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    </div>
+                    @include('components.validation-errors')
+                    <form class="user" action="{{ route('register') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="name" name="name" value="{{ old('name') ?? '' }}" class="form-control form-control-user"
+                                id="exampleInputName" aria-describedby="nameHelp"
+                                placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" value="{{ old('email') ?? '' }}" class="form-control form-control-user"
+                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                placeholder="Email Address">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control form-control-user"
+                                id="exampleInputPassword" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <input type="password_confirmation" name="password_confirmation" class="form-control form-control-user"
+                                id="exampleInputPassword" placeholder="Confirm Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                            Register
+                        </button>
+                    </form>
+                    <hr>
+                    <div class="text-center">
+                        <a class="small" href="{{ route('login') }}">Login in here!</a>
+                    </div>
+                </div>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+</div>
+@endsection
